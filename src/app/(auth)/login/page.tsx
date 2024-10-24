85f5ed4b87d1492ee68f747de1624fdbcb47ba89
Fedorenko <axiom_ua@yahoo.com>
2024-10-24 19:28:44 +0300

    create app

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import React, {FC, ReactElement} from 'react';
+
+const Page: FC = (): ReactElement => {
+  return (
+    <div>
+      Login
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/app/(auth)/registration/Page.tsx
@@ -0,0 +1,11 @@
+import React, {FC, ReactElement} from 'react';
+
+const Page: FC = (): ReactElement => {
+  return (
+    <div>
+      Page
+    </div>
+  );
+};
+
+export default Page;
